425f78a4b2e266a1a033789d602129f02a99f24c
Samuel Seyoum <samiseyoum@aol.com>
2018-05-23 22:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   } 
   
-   else if(string === "I love you, Grandma") {
+   else if(string === "I love you, Grandma.learn test
+   ") {
     return "I love you, too."
   }
 }
