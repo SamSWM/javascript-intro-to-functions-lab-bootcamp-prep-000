894ac8f9e51438fa3b60a19a0f5e66f598f248ac
Samuel Seyoum <samiseyoum@aol.com>
2018-05-23 16:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function shout(string) {
   return string.toUpperCase()
 }
 'Hello!'.toUpperCase() // 'HELLO!'
+function whisper(string) {
+  return string.toLowerCase() 
+}
+"Hi".toLowerCase() // 'hi grandma!'
