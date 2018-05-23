080dc24d689dcf0c4917c226a7ef3c5f86fea8e3
Samuel Seyoum <samiseyoum@aol.com>
2018-05-23 21:52:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } 
   
-  // else  if(string === matches string.toUppercase()){
+  else if(string === string.toUpperCase()){
     return "YES INDDEED!"
   } 
   
-  // else if(string === matches string.I love you, Grandma.()){
+   else if(string === "I love you, Grandma") {
     return "I love you, too."
   }
-  
-  
+}
+   
    
 
 
